d443ce9e22d1a4cfdd704f670aeb8a14e1110030
Camilo Forero <juan.foreror@utadeo.edu.co>
2026-02-27 19:13:20 -0500

    Add GitHub Pages workflow and Vite config

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    // Necesario para GitHub Pages. Debe coincidir con el nombre de tu repositorio.
+    base: '/sketchbook-virtual/',
+})
